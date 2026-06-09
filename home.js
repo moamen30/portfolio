f8799a015c5e4e65d704c7e3c7606e3b4aa08b78
moamen30 <m90140492@gmail.com>
2026-06-09 12:23:15 +0200

    Initial upload

---
+++ home.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typing = new Typed('#s4', {
+    strings: ['Web Developer (Front-End)', 'Computer Scientist','designer', 'programmer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+  });
+});
